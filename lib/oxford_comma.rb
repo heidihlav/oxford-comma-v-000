d554299b0eace50f3000a7a4ca39e2655679ac96
Heidi Hlavinka <hchlav@gmail.com>
2018-03-11 21:59:47 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,5 @@
 def oxford_comma(array)
-  return array.join("and") if array.length <= 2
+  return array.join(" and ") if array.length <= 2
   array[0..-2].join(", ") + ", and " + array[-1]
   # array.join(" and ")
 end
